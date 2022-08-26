0271587b8de5fc124ddf0e157181c8a1c3da644a
jefft0 <jeff@thefirst.org>
2022-08-26 06:09:09 -0700

    main.swift: Clarify error description.

--- swift-objc-data/main.swift
+++ swift-objc-data/main.swift
@@ -3,6 +3,6 @@ let obj = TestObject()
 obj.get_data_pointer_without_error()
 print("get_data_pointer_without_error returned")
 
-// This throws an error:
+// This crashes on returning from Objective-C without returning a Swift exception.:
 try obj.get_data_pointer_with_error()
 print("get_data_pointer_with_error returned")
