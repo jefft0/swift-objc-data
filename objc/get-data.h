c64119b78daeeb2420e5615cb7caad958480604e
jefft0 <jeff@thefirst.org>
2022-08-27 00:22:56 -0700

    Add attribute nonnull_error.

--- objc/get-data.h
+++ objc/get-data.h
@@ -3,6 +3,6 @@
 @interface TestObject : NSObject
 
 - (NSData* _Nullable)get_data_pointer_without_error;
-- (NSData* _Nullable)get_data_pointer_with_error:(NSError* _Nullable* _Nullable)error;
+- (NSData* _Nullable)get_data_pointer_with_error:(NSError* _Nullable* _Nullable)error __attribute__((swift_error(nonnull_error)));
 
 @end
